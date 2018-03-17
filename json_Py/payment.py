25e86ebfa393beeb603fa0e3dd088d573425a11a
Zelin <wangzl1116@mails.jlu.edu.cn>
2018-03-17 16:22:59 +0800

    补贴完成

--- json_Py/payment.py
+++ json_Py/payment.py
@@ -4,7 +4,7 @@ import numpy as np
 import random
 import pandas as pd
 from datetime import datetime
-
+import copy
 
 
 # shop_arr 普通商店
@@ -72,8 +72,13 @@ def MakeOnePayment(DateTime,Onumber,Inumber,Money):
     tmp_hour = RandomHour()
     tmp_min  = RandomMinAndSec()
     tmp_sec  = RandomMinAndSec()
-    tmp_tag  = DateTime[0:4] +DateTime[5:7]+ DateTime[8:10]+str(tmp_hour*10000+tmp_min*100+tmp_sec) \
-                + str(Onumber) + str(Inumber) + str(Money)
+    money_str = str(Money).zfill(5)
+    hour_str = str(tmp_hour).zfill(2)
+    min_str = str(tmp_min).zfill(2)
+    sec_str = str(tmp_sec).zfill(2)
+    
+    tmp_tag  = DateTime[0:4] +DateTime[5:7]+ DateTime[8:10]+  hour_str + min_str + sec_str \
+                + str(Onumber) + str(Inumber) +  money_str
     return {'year' : int(DateTime[0:4]),
             'month': int(DateTime[5:7]),
             'day': int(DateTime[8:10]),
@@ -86,6 +91,22 @@ def MakeOnePayment(DateTime,Onumber,Inumber,Money):
             'tag':tmp_tag}
 
 
+def MakeOneAllowance(OnePaymentDict,PaymentList):
+    tmp_pay_dict = copy.deepcopy(OnePaymentDict)
+    tmp_pay_dict['sec'] = tmp_pay_dict['sec'] + 1
+
+    tmp_pay_dict['onumber'] =  int(9002)
+    tmp_pay_dict['inumber'] =  OnePaymentDict['onumber']
+    tmp_pay_dict['money'] =   int(5)
+    money_str = str(5).zfill(5)
+    tmp_pay_dict['tag'] = OnePaymentDict['tag'][0:12]+ str(tmp_pay_dict['sec']) \
+                + str (tmp_pay_dict['onumber']) + str(tmp_pay_dict['inumber']) \
+                + money_str
+    PaymentList.append(tmp_pay_dict)
+    return
+
+
+
 def datelist(beginDate, endDate):
     # beginDate, endDate是datetime格式
     date_l=[datetime.strftime(x,'%Y-%m-%d') \
@@ -96,7 +117,7 @@ def datelist(beginDate, endDate):
 
 
 
-whole_date_arr = datelist('2017-01-01','2017-12-31')
+whole_date_arr = datelist('2017-01-01','2017-01-03')
 
 def one_stu_whole_payments(stu_index):
     one_stu_din_shop_money = [0 for x in range(len(whole_date_arr))]
@@ -128,11 +149,82 @@ def one_stu_whole_payments(stu_index):
             one_stu_payment.append(tmp_payment)
     return one_stu_payment
 
+
+
+#教职工消费记录
+
+def one_whole_payments_with_allowence(index,arr):
+    one_tea_din_shop_money = [0 for x in range(len(whole_date_arr))]
+    one_tea_payment =[]
+
+    for i in range(len(whole_date_arr)):
+        Temp_Dinning_Money =  RandomMoneyDinning()
+        while True:
+            if (Temp_Dinning_Money + one_tea_din_shop_money[i] < 10000):
+                tmp_payment = MakeOnePayment(whole_date_arr[i],arr[index],
+                                int(random.choice(din_arr)),Temp_Dinning_Money)
+                one_tea_din_shop_money[i] +=Temp_Dinning_Money
+                one_tea_payment.append(tmp_payment)
+                if(Temp_Dinning_Money > 2000):
+                    MakeOneAllowance(tmp_payment,one_tea_payment)
+            
+                Temp_Shopping_Money = RamdomMoneyShop()
+                if(Temp_Shopping_Money + one_tea_din_shop_money[i] < 10000):
+                    tmp_payment = MakeOnePayment(whole_date_arr[i],arr[index],
+                                int(random.choice(shop_arr)),Temp_Shopping_Money)
+                    one_tea_din_shop_money[i] +=Temp_Shopping_Money
+                    one_tea_payment.append(tmp_payment)
+                else:
+                    break
+            else:
+                break
+        if(random.randint(0,1)):
+            Temp_Bath_Money = RandomMoneyBath()
+            tmp_payment = MakeOnePayment(whole_date_arr[i],arr[index],
+                                int(random.choice(bath_arr)),Temp_Bath_Money)
+            one_tea_payment.append(tmp_payment)
+    return one_tea_payment
+
+
 all_stu_payment = []
 for i in range(len(stu_arr)):
     one_stu = one_stu_whole_payments(i)
     all_stu_payment.extend(one_stu)
 
 
-with open('ALL_STU_PAYMENTS.json', 'a') as outfile:
+
+all_tea_payment = []
+for i in range(len(tea_arr)):
+    one_tea = one_whole_payments_with_allowence(i,tea_arr)
+    all_tea_payment.extend(one_tea)
+
+all_emp_payment = []
+for i in range(len(emp_arr)):
+    one_emp = one_whole_payments_with_allowence(i,emp_arr)
+    all_emp_payment.extend(one_emp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+with open('ALL_STU_PAYMENTS.json', 'w') as outfile:
     json.dump(all_stu_payment, outfile, ensure_ascii=False,indent=2)
+
+with open('ALL_TEA_PAYMENTS.json', 'w') as outfile:
+    json.dump(all_tea_payment, outfile, ensure_ascii=False,indent=2)
+
+with open('ALL_EMP_PAYMENTS.json', 'w') as outfile:
+    json.dump(all_emp_payment, outfile, ensure_ascii=False,indent=2)
+
