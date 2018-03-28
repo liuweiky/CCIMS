0a9194464d8219eeb35006d0993872f5c71412cf
jetfire948 <382787300@qq.com>
2018-03-28 15:43:46 +0800

    Revert "Solve Conflicts"
    This reverts commit 48093b7798ef81052ba39d0a3d1fa5628321e5cc.

---
+++ info_table_BASE_11204.h
@@ -0,0 +1,65 @@
+#ifndef INFO_TABLE_H
+#define INFO_TABLE_H
+#include <QTableWidgetItem>
+#include <QWidget>
+#include <QTableWidget>
+#include "ccmis.h"
+#include <QButtonGroup>
+#include <QCheckBox>
+#include "information.h"
+#include <QDateTimeEdit>
+namespace Ui {
+class Info_Table;
+}
+
+class Info_Table : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Info_Table(CCMIS* ccmis_system,QWidget *parent = 0);
+    ~Info_Table();
+    void PrintOneInfo(CCMIS* ccmis_system, Information* OneLineInfo,int  IndexOfRow);
+
+    //只显示一个人的补助表格
+    void GetWholeOneUserSubsidyTable(QTableWidget* qtable,int inum);
+
+    //同一个入帐账号 表格
+    void GetWholeOneUserSearchTable(QTableWidget* qtable,int onum);
+    void ShowSameOnumOneInfo(QTableWidget *qtable,Information* one_info,int& row_index);
+
+
+
+    //同一个出账账号 表格
+    void ShowSameInumOneInfo(QTableWidget *qtable,Information* one_info, int &row_index);
+     void GetWholeOneShopSearchTable(QTableWidget* qtable, int inum);
+
+
+     //日期控件 设置
+     void SetStartFinishRange(QDateEdit* start_edit,QDateEdit* finish_edit);
+
+
+     void Table_Filtered_By_Date(QTableWidget* table,QDate* start_date ,
+                                                     QDate* finish_date);
+private:
+    Ui::Info_Table *ui;
+    CCMIS* mCCMIS;
+
+    QCheckBox* CheckForSubsidy;
+    QDateEdit* Start_Date_Edit;
+    QDateEdit* Finish_Date_Edit;
+
+    QDate* Start_Date;
+    QDate* Finish_Date;
+
+public slots:
+
+    void on_Check_Button_StateChoose(int state);
+
+   void onStartDateChanged(const QDate &date);
+   void onFinishDateChanged(const QDate &date);
+private slots:
+   void on_Search_clicked();
+};
+
+#endif // INFO_TABLE_H
---
+++ info_table_REMOTE_4036.cpp
@@ -0,0 +1,307 @@
+#include "info_table.h"
+#include "ui_info_table.h"
+#include "user.h"
+#include "shop.h"
+#include <QDebug>
+Info_Table::Info_Table(CCMIS* ccmis_system,QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Info_Table)
+{
+    mCCMIS = ccmis_system;
+    ui->setupUi(this);
+    int user_num = mCCMIS->GetUserNum();
+    //设置按钮
+
+    Start_Date = new QDate();
+    Finish_Date = new QDate();
+
+    CheckForSubsidy = ui->checkBox;
+    CheckForSubsidy->setText("显示补助");
+    if(user_num >= CCMIS::USER_TEA_EMP_BEGIN
+            && user_num <= CCMIS::USER_TEA_EMP_END)
+        CheckForSubsidy->setCheckable(true);
+    else
+        CheckForSubsidy->setCheckable(false);
+
+    connect(CheckForSubsidy,SIGNAL(stateChanged(int)),SLOT(on_Check_Button_StateChoose(int)));
+
+
+
+    //日期筛选
+    //Start_Date_Edit = new QDateEdit(this);
+    //Finish_Date_Edit = new QDateEdit(this);
+    Start_Date_Edit = ui->start;
+    Finish_Date_Edit = ui->finish;
+    SetStartFinishRange(Start_Date_Edit,Finish_Date_Edit);
+    connect(Start_Date_Edit,SIGNAL(dateChanged(QDate)),
+            SLOT(onStartDateChanged(QDate)));
+    connect(Finish_Date_Edit,SIGNAL(dateChanged(QDate)),
+            SLOT(onFinishDateChanged(QDate)));
+
+
+    connect(ui->toExcelButton,SIGNAL(pressed()),
+            SLOT(on_toExcelButton_clicked()));
+    connect(ui->Search,SIGNAL(clicked(bool)),
+            SLOT(on_Search_clicked()));
+    connect(ui->Reset,SIGNAL(clicked(bool)),
+            SLOT(on_Reset_clicked()));
+
+    GetWholeOneUserSearchTable( ui->tableWidget,user_num);
+    //Table_Filtered_By_Date(ui->tableWidget, new QDate(2018,2,10), new QDate(2018,2,15));
+}
+
+
+
+
+void Info_Table::SetStartFinishRange(QDateEdit* start_edit,QDateEdit* finish_edit)
+{
+    QDate start_date(2018,1,1);
+
+
+
+    //起始时间范围限制
+    start_edit->setDate(start_date);
+    start_edit->setMaximumDate(QDate::currentDate());
+    start_edit->setMinimumDate(start_date);
+
+    //结束时间范围限制
+    finish_edit->setDate(QDate::currentDate());
+    finish_edit->setMaximumDate(QDate::currentDate());
+    finish_edit->setMinimumDate(start_date);
+
+
+    //弹出框
+    start_edit->setCalendarPopup(true);
+    finish_edit->setCalendarPopup(true);  // 日历弹出
+}
+
+void Info_Table::onStartDateChanged(const QDate &date)
+{
+   qDebug() << "StartDateTime : " << date;
+    this->Start_Date->setDate(date.year(),date.month(),date.day());
+
+    this->Finish_Date_Edit->setMinimumDate(date);
+
+}
+
+void Info_Table::onFinishDateChanged(const QDate &date)
+{
+    qDebug() << "FinishDateTime : " << date;
+    this->Finish_Date->setDate(date.year(),date.month(),date.day());
+    qDebug() << "FinishDateTime : " << date;
+    this->Start_Date_Edit->setMaximumDate(date);
+
+}
+
+
+
+
+void Info_Table::on_Check_Button_StateChoose(int state)
+{
+    switch (state) {
+    case Qt::Checked:
+         GetWholeOneUserSubsidyTable(ui->tableWidget,mCCMIS->GetUserNum());
+        break;
+    case Qt::Unchecked:
+        GetWholeOneUserSearchTable( ui->tableWidget,mCCMIS->GetUserNum());
+        break;
+
+    }
+}
+
+
+
+
+
+
+Info_Table::~Info_Table()
+{
+    delete ui;
+}
+
+
+
+//补助表格
+void Info_Table::GetWholeOneUserSubsidyTable(QTableWidget* qtable,int inum)
+{
+    qtable->clear();
+    //设置表头
+    qtable->setColumnCount(5);
+    qtable->setHorizontalHeaderLabels(
+                QStringList()<<"日期" <<"时间" <<"入帐号"<<"被补助人"<<"金额");
+    //设置列宽
+    qtable->setColumnWidth(0,120);
+    qtable->setColumnWidth(1,100);
+    qtable->setColumnWidth(2,50);
+    qtable->setColumnWidth(3,150);
+    qtable->setColumnWidth(4,100);
+
+
+    int RowCount = 0;
+
+    Information* iter = mCCMIS->GetInfoPointer() ->next;
+
+    while(iter!=NULL){
+        if( (iter->Onumber == 2) && (iter->Inumber == inum))
+            ShowSameOnumOneInfo(qtable,iter,RowCount);
+        iter  = iter->next;
+
+    }
+
+    return;
+}
+
+
+void Info_Table::GetWholeOneUserSearchTable(QTableWidget* qtable,int onum)
+{
+     qtable->clear();
+   //设置表头
+   qtable->setColumnCount(5);
+   qtable->setHorizontalHeaderLabels(
+               QStringList()<<"日期" <<"时间" <<"入帐号"<<"商铺名"<<"金额");
+   //设置列宽
+   qtable->setColumnWidth(0,120);
+   qtable->setColumnWidth(1,100);
+   qtable->setColumnWidth(2,50);
+   qtable->setColumnWidth(3,150);
+   qtable->setColumnWidth(4,100);
+
+
+   int RowCount = 0;
+
+   Information* iter = mCCMIS->GetInfoPointer() ->next;
+
+   while(iter!=NULL){
+       if(iter->Onumber == onum)
+           ShowSameOnumOneInfo(qtable,iter,RowCount);
+       iter  = iter->next;
+
+   }
+
+   return;
+
+
+
+}
+
+void Info_Table::ShowSameOnumOneInfo(QTableWidget *qtable,Information* one_info,int& row_index)
+{
+   QString Name = mCCMIS->GetAllNameByNum(one_info->Inumber);
+   QDateTime info_date_time = Information::InfoToDateTime(one_info);
+
+   QString Date = info_date_time.toString("yyyy-MM-dd");
+
+
+   QString Time = info_date_time.toString("hh:mm:ss");
+   QString Money = Information::InfoToMoneyStr(one_info);
+   qtable->insertRow(row_index);
+
+
+   qtable->setItem(row_index,0,new QTableWidgetItem(Date));
+   qtable->setItem(row_index,1,new QTableWidgetItem(Time));
+   qtable->setItem(row_index,2,
+                       new QTableWidgetItem(QString::number(one_info->Inumber)));
+
+   qtable->setItem(row_index,3,new QTableWidgetItem(Name));
+   qtable->setItem(row_index,4,new QTableWidgetItem(Money));
+   row_index++;
+}
+
+
+
+
+
+
+
+
+ void Info_Table::GetWholeOneShopSearchTable(QTableWidget* qtable,int inum)
+{
+     qtable->clear();
+    //设置表头
+    qtable->setColumnCount(5);
+    qtable->setHorizontalHeaderLabels(
+                QStringList()<<"日期" <<"时间" <<"卡号"<<"姓名"<<"金额");
+    //设置列宽
+    qtable->setColumnWidth(0,120);
+    qtable->setColumnWidth(1,100);
+    qtable->setColumnWidth(2,50);
+    qtable->setColumnWidth(3,100);
+    qtable->setColumnWidth(4,100);
+
+
+    int RowCount = 0;
+
+    Information* iter = mCCMIS->GetInfoPointer() ->next;
+
+    while(iter!=NULL){
+        if(iter->Inumber == inum)
+            ShowSameInumOneInfo(qtable,iter,RowCount);
+        iter  = iter->next;
+
+    }
+
+    return;
+
+
+
+}
+
+void Info_Table::ShowSameInumOneInfo(QTableWidget *qtable,Information* one_info,int& row_index)
+{
+    QString UserName = mCCMIS->GetAllNameByNum(one_info->Onumber);
+    QString Date = Information::InfoToDateStr(one_info);
+    QString Time = Information::InfoToTimeStr(one_info);
+    QString Money = Information::InfoToMoneyStr(one_info);
+    qtable->insertRow(row_index);
+
+
+    qtable->setItem(row_index,0,new QTableWidgetItem(Date));
+    qtable->setItem(row_index,1,new QTableWidgetItem(Time));
+    qtable->setItem(row_index,2,
+                        new QTableWidgetItem(QString::number(one_info->Onumber)));
+
+    qtable->setItem(row_index,3,new QTableWidgetItem(UserName));
+    qtable->setItem(row_index,4,new QTableWidgetItem(Money));
+    row_index++;
+}
+
+
+void Info_Table::on_Search_clicked()
+{
+    Table_Filtered_By_Date(ui->tableWidget, Start_Date,Finish_Date);
+}
+
+void Info_Table::Table_Filtered_By_Date(QTableWidget* table,QDate* start_date,QDate* finish_date)
+{
+
+    for( int i = 0; i < table->rowCount(); ++i )
+    {
+        bool match = false;
+        QTableWidgetItem *item = table->item( i, 0 );
+        QDate one_date = QDate::fromString (item->text(),"yyyy-MM-dd" );
+        //qDebug()<<one_date;
+        if( (one_date >= (*start_date)) && (one_date <= (*finish_date)))
+
+        { qDebug()<<one_date;
+            match = true;
+           // break;
+        }
+        table->setRowHidden( i, !match );
+     }
+
+}
+
+
+
+
+void Info_Table::on_toExcelButton_clicked()
+{
+    QString filename(mCCMIS->GetCurrentUserName());
+
+    COMMON_FUNCS::Table2Excel(ui->tableWidget,filename+"的消费信息");
+}
+
+void Info_Table::on_Reset_clicked()
+{
+    GetWholeOneUserSearchTable( ui->tableWidget,mCCMIS->GetUserNum());
+}
