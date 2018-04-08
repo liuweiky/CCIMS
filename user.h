a760a988bc37866df9c43025b5a444b788e5abf6
Kingrealer <1005031096@qq.com>
2018-04-08 11:30:16 +0800

    Add system organization graph (MindManager)

---
+++ user.h
@@ -0,0 +1,17 @@
+#ifndef USER_H
+#define USER_H
+
+#include <QString>
+#include <string>
+
+struct User
+{
+    int         number;
+    QString     name;
+    std::string password;
+    int         balance;    //余额
+    int         coupon;     //劵
+    User*       next;       //下个结点
+};
+
+#endif // USER_H
